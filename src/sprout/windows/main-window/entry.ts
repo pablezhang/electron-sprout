0c644119668d972b119233c94cbe9b8ef02b179f
pikun <1457431899@qq.com>
2019-12-22 10:40:52 +0800

    remove:移除多余文件index.html

---
+++ src/sprout/windows/main-window/entry.ts
@@ -0,0 +1,3 @@
+// import 'sprout/windows/main-window/mainwindow.initservices';
+// import { startMainWindowRender } from 'sprout/windows/main-window/electron-render/index';
+// startMainWindowRender();
--- src/sprout/windows/main-window/main-window.js
+++ src/sprout/windows/main-window/main-window.js
@@ -1,11 +1,13 @@
 //@ts-check
 'use strict';
 
+
+
 const bootstrapWindow = require('../../../bootstrap-window');
 
 console.log('main-window service');
 bootstrapWindow.load([
-	'sprout/windows/main-window/mainwindow.initservices',
+	'sprout/windows/main-window/mainwindow.initservices'
 ], (mainWindow, configuration) => {
 	// @ts-ignore
 	return require('sprout/windows/main-window/index').main(configuration).then(() => {
